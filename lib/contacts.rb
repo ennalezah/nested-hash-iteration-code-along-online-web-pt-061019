1b45a1d893c2ce108b5b935261405b0da6d9496e
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-26 22:57:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -42,7 +42,7 @@ def all_flavors(contacts)
   contacts.map do |person, details|
     details.map do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-        values.values.flatten
+        value.values.flatten
       end
     end
   end
